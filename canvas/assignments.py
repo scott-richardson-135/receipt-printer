3e7d46d0b19b01a7e3d1634478dc8178b58c5b9a
Scott Richardson <srich1550@gmail.com>
2025-08-18 14:28:33 -0600

    added function to find assignments due within a week

--- canvas/assignments.py
+++ canvas/assignments.py
@@ -1,4 +1,23 @@
 from .api import canvas_get
+from datetime import datetime, timedelta, timezone
 
 def get_assignments(course_id):
     return canvas_get(f"courses/{course_id}/assignments")
+
+
+# takes in a list of all assignments and returns a list of assignments due within a week
+def due_within_7_days(assignments):
+    now = datetime.now(timezone.utc)
+    upcoming_assignments = []
+
+    for assignment in assignments:
+        due_at = assignment.get("due_at")
+        if due_at:
+            #convert from the format that canvas sends it in to the one that timedelta wants
+            due = datetime.fromisoformat(due_at.replace("Z", "+00:00"))
+
+            if now <= due <= now + timedelta(days=7):
+                upcoming_assignments.append(assignment) 
+
+    return upcoming_assignments
+
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from printer.actions import print_custom_message
 from canvas.courses import get_active_courses
-from canvas.assignments import get_assignments
+from canvas.assignments import get_assignments, due_within_7_days
 
 if __name__ == "__main__":
     #print_custom_message("Sometimes I wish I was gurt\n")
@@ -13,9 +13,9 @@ if __name__ == "__main__":
         if name:
             print(name)
 
-        course_id = course.get("id")
         assignments = get_assignments(course_id)
-        for assignment in assignments:
+        filtered_assignments = due_within_7_days(assignments)
+        for assignment in filtered_assignments:
             assignment_name = assignment.get("name")
             if assignment_name:
                 print(assignment_name)
