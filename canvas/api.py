1b5705d3a31222076fb28b387171da1468818a5c
Scott Richardson <srich1550@gmail.com>
2025-08-18 13:50:38 -0600

    fixed requests to loop for pagination

--- canvas/api.py
+++ canvas/api.py
@@ -3,6 +3,16 @@ from .config import headers, school_path
 
 def canvas_get(endpoint, params=None):
     url = f"{school_path}api/v1/{endpoint}"
-    response = requests.get(url, headers=headers, params=params)
-    response.raise_for_status()
-    return response.json()
+    results = []
+    
+    #while loop for pagination
+    while url:
+        response = requests.get(url, headers=headers, params=params)
+        response.raise_for_status()
+        results.extend(response.json())
+
+        # get the link labeled "next" in the response, if it doesn't exist we return None
+        url = response.links.get("next", {}).get("url") 
+
+
+    return results
