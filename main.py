32f7c374843009cd776ba905899c8b358643afdf
Scott Richardson <srich1550@gmail.com>
2025-08-18 22:12:02 -0600

    added function to print assignment as receipt

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from printer.actions import print_custom_message
+from printer.actions import print_custom_message, print_as_receipt
 from canvas.courses import get_active_courses
 from canvas.assignments import get_assignments, due_within_7_days
 
 if __name__ == "__main__":
-    print_custom_message("Sometimes I wish I was gurt\n")
 
     courses = get_active_courses()
     
@@ -12,11 +11,11 @@ if __name__ == "__main__":
         course_id = course.get("id")
         if name:
             print(name)
-
+        print(course_id)
         assignments = get_assignments(course_id)
         filtered_assignments = due_within_7_days(assignments)
-        for assignment in filtered_assignments:
-            assignment_name = assignment.get("name")
-            if assignment_name:
-                print(assignment_name)
+
+
+        for assignment in assignments[:2]:
+            print_as_receipt(assignment, name)
 
--- printer/actions.py
+++ printer/actions.py
@@ -6,4 +6,19 @@ def print_custom_message(message):
     printer.text(message)
     printer.cut()
     printer.close()
-    
+
+
+def print_as_receipt(assignment, course_name):
+    title = assignment.get("name", "Untitled")
+    due = assignment.get("due_at", "No due date")
+    course = course_name
+    points = assignment.get("points_possible", "Mysterious points")
+
+    print_custom_message(
+        "<------------------------->\n"
+        f"Course: {course}\n\n"
+        f"Assignment: {title}\n\n"
+        f"Due at: {due}\n\n"
+        f"Points possible: {points}\n\n"
+        "<------------------------->\n"
+    )
