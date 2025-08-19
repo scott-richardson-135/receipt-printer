0b29fadaa7743cb7cf154d3454e8750491e4e079
Scott Richardson <srich1550@gmail.com>
2025-08-18 22:53:08 -0600

    removed debugging stuff

--- main.py
+++ main.py
@@ -12,14 +12,12 @@ if __name__ == "__main__":
     for course in courses:
         name = course.get("name")
         course_id = course.get("id")
-        if name:
-            print(name)
-        print(course_id)
+
         assignments = get_assignments(course_id)
         filtered_assignments = due_within_7_days(assignments)
 
 
-        for assignment in assignments[:2]:
+        for assignment in filtered_assignments:
             aid = assignment.get("id")
             if aid not in printed_ids:
                 print_as_receipt(assignment, name)
